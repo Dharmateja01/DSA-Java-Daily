aeeb6a874a911ce7a7e41273cf8b90de9b449694
Dharmateja01 <pandugadharmateja05@gmail.com>
2026-07-31 19:45:55 +0530

    Linear Search The element

---
+++ src/arrays/LinearSearchElement.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class LinearSearchElement {
+    private static int linearsearch(int[] nums, int target) {
+        for(int i=0;i< nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4,5,6,7,8};
+        int target=50;
+        int result=linearsearch(nums,target);
+        if(result !=-1){
+            System.out.println("element found at index : "+result);
+        }else{
+            System.out.println("element not found ");
+        }
+    }
+}
